7dd2c772e22408c734e2813db0bde094ee466150
hridoypboss <hridoydakua1994@gmail.com>
2026-01-13 22:33:28 +0600

    array add

--- array.js
+++ array.js
@@ -1,20 +1,2 @@
-var frindeAge = [12, 14, 16, 18];
-
-var sonaliAge = frindeAge[3];    // ১৮ নম্বর ইনডেক্সের মান নেদীদা হচ্ছে
-
-frindeAge [0] = 20; // নতুন মান দিয়ে আপসেট বরা হচ্ছে
-
-var position = frindeAge.indexOf(16);   // পলিসন দেখা
-
-
-console.log(frindeAge);  
-
-frindeAge.push(22); // সেশে এভটি উপাদার যো গ বরা হচ্ছে
-
-frindeAge.push(24);
-
-console.log(frindeAge); 
-
-frindeAge.pop();             // পিচন থেকে এভটি উপাদার বাধ দেওয়া হচ্ছে
-
-console.log(frindeAge);
+var a = ['kalam, clam, kudus, korim']
+console.log(a)
