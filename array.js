f538a6e2e080fe7fc260a54660947bd9d270440a
hridoypboss <hridoydakua1994@gmail.com>
2026-01-13 23:15:56 +0600

    3 array add

--- array.js
+++ array.js
@@ -1,2 +1,6 @@
-var a = ['kalam, clam, kudus, korim']
-console.log(a)
+var a = ['kalam', 'khan', 'khan', 'raton'];
+
+a.unshift('lalu');  // সবার আগে যোগ করে
+var  B = a.slice(3);
+console.log(B);
+console.log(a);
