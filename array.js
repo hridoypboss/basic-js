680ca27d3f390070b0e80c8aaabe17027110999f
hridoypboss <hridoydakua1994@gmail.com>
2026-01-13 22:28:59 +0600

    2 array add

--- array.js
+++ array.js
@@ -1,9 +1,20 @@
 var frindeAge = [12, 14, 16, 18];
 
-var sonaliAge = frindeAge[3];    // ১৮ নম্বর ইনডেক্সের মান নেওয়া হচ্ছে
+var sonaliAge = frindeAge[3];    // ১৮ নম্বর ইনডেক্সের মান নেদীদা হচ্ছে
 
-frindeAge [0] = 20; // নতুন মান দিয়ে আপডেট করা হচ্ছে
+frindeAge [0] = 20; // নতুন মান দিয়ে আপসেট বরা হচ্ছে
 
-var position = frindeAge.indexOf(16);   // পজিসন দেখা
+var position = frindeAge.indexOf(16);   // পলিসন দেখা
 
-console.log(position);  
+
+console.log(frindeAge);  
+
+frindeAge.push(22); // সেশে এভটি উপাদার যো গ বরা হচ্ছে
+
+frindeAge.push(24);
+
+console.log(frindeAge); 
+
+frindeAge.pop();             // পিচন থেকে এভটি উপাদার বাধ দেওয়া হচ্ছে
+
+console.log(frindeAge);
