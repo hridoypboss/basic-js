89d4de79ecff09f18a86de26de4c14dcc36106e9
hridoypboss <hridoydakua1994@gmail.com>
2026-01-13 21:26:34 +0600

    first js commit

---
+++ array.js
@@ -0,0 +1,9 @@
+var frindeAge = [12, 14, 16, 18];
+
+var sonaliAge = frindeAge[3];    // ১৮ নম্বর ইনডেক্সের মান নেওয়া হচ্ছে
+
+frindeAge [0] = 20; // নতুন মান দিয়ে আপডেট করা হচ্ছে
+
+var position = frindeAge.indexOf(16);   // পজিসন দেখা
+
+console.log(position);  
---
+++ hello.js
@@ -0,0 +1,3 @@
+var a = 5.99;
+var total = Math.floor(a);
+console.log( total );
