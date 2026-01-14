a209c6c90322e285e6f99c945609098ef3f054bf
hridoypboss <hridoydakua1994@gmail.com>
2026-01-14 16:43:18 +0600

    of

--- array.js
+++ array.js
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-var a = 20;
-if(a>=20){  // এর থেকে বড় হলে true আর না হলে false
-    console.log("a is greater than 20");
-
-}else{
-    console.log("a is equal to 20");
-}
-=======
-console.log("hello world");
->>>>>>> 8b85dd721705fc611f8e2712c876651886dca45c
+console.log(5+6)
