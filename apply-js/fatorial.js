046c6fb3fddbcbd24b21045d1e13da4aaffa3265
hridoypboss <hridoydakua1994@gmail.com>
2026-01-18 17:26:59 +0600

    message

---
+++ apply-js/fatorial.js
@@ -0,0 +1,20 @@
+// ফ্যাক্টোরিয়াল বের করার ফাংশন
+function factorial(n) {                // n এর ফ্যাক্টোরিয়াল বের করার জন্য ফাংশন
+    var factorial = 1;                // 1 থেকে n পর্যন্ত গুণফল বের করার জন্য লুপ চালানো হবে
+    for (var i = 1; i <= n; i++) {   // লুপ শুরু
+        factorial = factorial * i;  // গুণফল বের করা হচ্ছে
+
+    }
+    return factorial;             // ফ্যাক্টোরিয়াল রিটার্ন করা হচ্ছে
+}
+console.log(factorial(5));  // আউটপুট: 120
+console.log(factorial(7));  // আউটপুট: 5040
+
+// while লুপ ব্যবহার করে ফ্যাক্টোরিয়াল বের করার ফাংশন
+var i = 1;
+var factorial = 1;
+wile(i <= 10) {
+    factorial = factorial * i;
+    i++;
+}
+console.log(factorial);
